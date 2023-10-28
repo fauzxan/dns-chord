466dddd66ce8486bd485e3fe4947190b918a3751
Fauzaan <fauzaan.mm@gmail.com>
2023-10-28 22:11:33 +0800

    Base protobuf

--- message/message.go
+++ message/message.go
@@ -1,30 +1,31 @@
 package message
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-// Sample message structure. To be replaced with a struct for protobuff
-type RequestMessage struct{
-	Type string // PING | SYNC | ACK | FIND_SUCCESSOR | CLOSEST_PRECEDING_NODE
-	TargetId uint64
-}
+// // Sample message structure. To be replaced with a struct for protobuff
+// type RequestMessage struct{
+// 	Type string // PING | SYNC | ACK | FIND_SUCCESSOR | CLOSEST_PRECEDING_NODE
+// 	TargetId uint64
+// 	IP string
+// }
 
-type ResponseMessage struct {
-	Type string
-	Nodeid uint64
-	IP string
-}
+// type ResponseMessage struct {
+// 	Type string
+// 	Nodeid uint64
+// 	IP string
+// }
 
-/*
-***************************************
-		UTILITY FUNCTIONS
-***************************************	
-*/
-func (msg *RequestMessage) PrintContent(){
-	fmt.Println("Message content:", msg)
-}
+// /*
+// ***************************************
+// 		UTILITY FUNCTIONS
+// ***************************************	
+// */
+// func (msg *RequestMessage) PrintContent(){
+// 	fmt.Println("Message content:", msg)
+// }
 
-func (msg *ResponseMessage) PrintContent(){
-	fmt.Println("Message content:", msg)
-}
+// func (msg *ResponseMessage) PrintContent(){
+// 	fmt.Println("Message content:", msg)
+// }
